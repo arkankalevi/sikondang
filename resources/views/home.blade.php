d221e5b28b1509e7d4d48accc1057cc8ee824e66
Arkan Kalevi <kaleviarkan@gmail.com>
2024-07-21 16:10:12 -1200

    membuat landing page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <title>SIKONDANG</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+
+          <!-- Favicons -->
+        <link href="home/img/logo.png" rel="icon">
+        <link href="home/img/logo.png" rel="logo">
+
+      
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com" rel="preconnect">
+        <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+      
+        <!-- Vendor CSS Files -->
+        <link href="home/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="home/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="home/vendor/aos/aos.css" rel="stylesheet">
+        <link href="home/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="home/vendor/swiper/swiper-bundle.min.css" rel="stylesheet"> 
+      
+        <!-- Main CSS File -->
+        <link href="home/css/style.css" rel="stylesheet">
+      
+        
+      </head>
+<body class="index-page">
+
+    <header id="header" class="header d-flex align-items-center fixed-top">
+        <div class="container-fluid container-xl position-relative d-flex align-items-center">
+    
+          <a href="home.blade.php" class="logo d-flex align-items-center me-auto">
+            <img src="home/img/logo.png" alt=""> 
+          </a>
+    
+          <nav id="navmenu" class="navmenu" >
+            <ul>
+              <li><a href="#hero" class="active">Home</a></li>
+              <li><a href="#organisasi">Organisasi</a></li>
+              <li><a href="#publikasi">Publikasi</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          </nav>
+    
+          <a class="btn-getstarted" href="#organisasi">Login</a>
+    
+        </div>
+      </header>
+
+      <main class="main">
+
+                 <!-- Hero Section -->
+    <section id="hero" class="hero  dark-background">
+
+        <div class="container">
+          <div class="row gy-4">
+            <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="zoom-out">
+              <h1>SIKONDANG</h1>
+              <p>Sistem Informasi Kota Serang Dalam Angka</p>
+              <div class="d-flex">
+                <a href="#about" class="btn-get-started">Selanjutnya</a>
+                
+              </div>
+            </div>
+            <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="200">
+              <img src="home/img/hero-img.png" class="img-fluid animated" alt="">
+            </div>
+          </div>
+        </div>
+  
+      </section>
+      <!-- /Hero Section -->
+
+      </main>
+
+      <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="home/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="home/vendor/php-email-form/validate.js"></script>
+  <script src="home/vendor/aos/aos.js"></script>
+  <script src="home/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="home/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="home/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="home/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="home/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="home/js/main.js"></script>
+
+        
+</body>
+</html>
