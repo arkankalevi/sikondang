65eb7dd0fa83ec6bbc62d7dca9a39023495b87a5
Arkan Kalevi <kaleviarkan@gmail.com>
2024-07-23 06:42:52 -1200

    menambahkah layanan,statistik pengunjung,footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,33 +11,31 @@
         <link href="home/img/logo.png" rel="icon">
         <link href="home/img/logo.png" rel="logo">
 
-      
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com" rel="preconnect">
         <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
       
-        <!-- Vendor CSS Files -->
+       <!-- Vendor CSS Files -->
+        <link href="home/vendor/aos/aos.css" rel="stylesheet">
         <link href="home/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="home/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-        <link href="home/vendor/aos/aos.css" rel="stylesheet">
         <link href="home/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-        <link href="home/vendor/swiper/swiper-bundle.min.css" rel="stylesheet"> 
-      
+        <link href="home/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+        
         <!-- Main CSS File -->
         <link href="home/css/style.css" rel="stylesheet">
-      
-        
       </head>
+
 <body class="index-page">
 
     <header id="header" class="header d-flex align-items-center fixed-top">
+
         <div class="container-fluid container-xl position-relative d-flex align-items-center">
-    
           <a href="home.blade.php" class="logo d-flex align-items-center me-auto">
             <img src="home/img/logo.png" alt=""> 
           </a>
-    
+          
           <nav id="navmenu" class="navmenu" >
             <ul>
               <li><a href="#hero" class="active">Home</a></li>
@@ -63,7 +61,7 @@
               <h1>SIKONDANG</h1>
               <p>Sistem Informasi Kota Serang Dalam Angka</p>
               <div class="d-flex">
-                <a href="#about" class="btn-get-started">Selanjutnya</a>
+                <a href="#services" class="btn-get-started">Selanjutnya</a>
                 
               </div>
             </div>
@@ -76,24 +74,145 @@
       </section>
       <!-- /Hero Section -->
 
-      </main>
+       <!-- Services Section -->
+    <section id="services" class="services section light-background">
 
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Layanan</h2>
+      </div><!-- End Section Title -->
+
+      <div class="container">
+        <div class="row gy-4">
+
+          <div class="col-xl-4 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-item position-relative">
+              <div class="icon"><i class="bi bi-activity icon"></i></div>
+              <h4><a href="" class="stretched-link">Statistik Sektoral</a></h4>
+              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+            </div>
+          </div><!-- End Service Item -->
+
+        
+          <div class="col-xl-4 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="300">
+            <div class="service-item position-relative">
+              <div class="icon"><i class="bi bi-calendar4-week icon"></i></div>
+              <h4><a href="" class="stretched-link">Perencanaan</a></h4>
+              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-xl-4 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="400">
+            <div class="service-item position-relative">
+              <div class="icon"><i class="bi bi-broadcast icon"></i></div>
+              <h4><a href="" class="stretched-link">Geospasial - Kota Serang</a></h4>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+            </div>
+          </div><!-- End Service Item -->
+        </div>
+      </div>
+
+    </section><!-- /Services Section -->
+
+     <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts">
+
+       <!-- Section Title -->
+       <div class="container section-title" data-aos="fade-up">
+        <h2>Stastistik Pengunjung</h2>
+      </div><!-- End Section Title -->
+
+      <div class="container">
+        <div class="row counters">
+
+          <div class="col-lg-4 col-6 text-center d-realtive">
+            <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
+            <h3>23</h3>
+            <p>Pengunjung Hari ini</p>
+          </div>
+
+          <div class="col-lg-4 col-6 text-center d-relative">
+            <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
+            <h3>541</h3>
+            <p>Pengunjung Bulan ini</p>
+          </div>
+
+          <div class="col-lg-4 col-6 text-center d-relative">
+            <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" class="purecounter"></span>
+            <h3>4.892</h3>
+            <p>Pengunjung Tahun Ini </p>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Counts Section -->
+
+  </main>
+
+
+  <footer id="footer" class="footer">
+    <div class="container footer-top">
+      <div class="row gy-4">
+        <div class="col-lg-5 col-md-6 footer-about">
+          <a href="index.html" class="d-flex align-items-center">
+            <span class="sitename">SIKONDANG</span>
+          </a>
+          <div class="footer-contact pt-3">
+            <p>Jl. Jend. Sudirman No.25, Banjaeagung, Sumurpecung, Kec. Serang, Kota Serang, Banten 42122</p>
+            <p class="mt-3"><strong>Phone:</strong> <span>0254221764</span></p>
+            <p><strong>Email:</strong> <span>diskominfoserangkota@gmail.com</span></p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 footer-links">
+          <h4>Our Services</h4>
+          <ul>
+            <li><i class="bi bi-chevron-right"></i> <a href="https://ragem.serangkota.go.id/">RAGEM</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="https://kominfo.serangkota.go.id/ ">Diskominfo Serang Kota</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="https://serangkota.go.id/">Smart city Serang Kota</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="https://serangkota.go.id/">Pemerintahan Serang Kota</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-4 col-md-12">
+          <h4>Follow Us</h4>
+          <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+          <div class="social-links d-flex">
+            <a href="https://x.com/pemkotserang"><i class="bi bi-twitter-x"></i></a>
+            <a href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/"><i class="bi bi-instagram"></i></a>
+            <a href="https://www.youtube.com/"><i class="bi bi-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container copyright text-center mt-4">
+      <p>
+        © <span>Copyright</span> 
+        <span>2019 - 2024 Sistem Informasi Kota Serang Dalam Angka version 3.0</span>
+      </p>
+      <div class="credits">
+        Designed by <a href="https://kominfo.serangkota.go.id/ ">IT E-gov Diskominfo Kota Serang</a>
+      </div>
+    </div>
+  </footer>
+
+      
       <!-- Scroll Top -->
   <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Preloader -->
   <div id="preloader"></div>
 
-  <!-- Vendor JS Files -->
-  <script src="home/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="home/vendor/php-email-form/validate.js"></script>
-  <script src="home/vendor/aos/aos.js"></script>
-  <script src="home/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="home/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="home/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="home/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="home/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-
+   <!-- Vendor JS Files -->
+   <script src="home/vendor/purecounter/purecounter_vanilla.js"></script>
+   <script src="home/vendor/aos/aos.js"></script>
+   <script src="home/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+   <script src="home/vendor/glightbox/js/glightbox.min.js"></script>
+   <script src="home/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+   <script src="home/vendor/swiper/swiper-bundle.min.js"></script>
+   <script src="home/vendor/php-email-form/validate.js"></script>
+ 
   <!-- Main JS File -->
   <script src="home/js/main.js"></script>
 
