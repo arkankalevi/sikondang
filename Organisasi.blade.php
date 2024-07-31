ae6a383b86a60742f3de0d38da17496ad86acd65
tahugenjreng99 <deiseranugrah@gmail.com>
2024-07-31 13:39:53 +0700

    Update Organisasi blade php
    penambahan fitur pencarian pada website

--- Organisasi.blade.php
+++ Organisasi.blade.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
     <title>Organisasi OPD - Sikondang</title>
     <style>
         body {
             font-family: Arial, sans-serif;
-            margin: 5px;
-            padding: 5px;
-            background-color: #3a80c5;
+            margin: 4px;
+            padding: 3px;
+            background-color: rgb(198, 199, 209);
         }
         .header {
             background-color: #06060c;
@@ -29,25 +29,26 @@
             list-style: none;   
             padding: 2px;
             margin: 2px;
-            text-align: center;
+            text-align: right;
         }
         .navbar ul li {
             display: inline;
-            margin: 0 15px;
+            margin:  15px;
         }
         .navbar ul li a {
             text-decoration: none;
-            color: #534b4b;
+            color: #f1eaea;
             font-weight: bold;
         }
         .breadcrumb {
             margin: 20px 0;
-            font-size: 14px;
-            color: #666;
-            text-align: center;
+            font-size: 10px;
+            color: #685e5e;
+            background-color: #474141
+            text-align: left;
         }
         .breadcrumb a {
-            color: #6dabe9;
+            color: #cad2db;
             text-decoration: none;
         }
         .title {
@@ -81,30 +82,82 @@
     </style>
 </head>
 <body>
-    <div class="container">
-    </div>
-    <div class="navbar">
-        <ul>
-            <img src="c:\Users\user\Pictures\Saved Pictures\government.png" class="img-rounded" alt="pemerintah kota serang">
+    <link rel="stylesheet" href="styles.css">
+    <nav class="navbar">
+        <div class="navbar-logo">
+            <img src="c:\Users\user\Pictures\Saved Pictures\banten.jpg">
+        </div>
+        <ul class="navbar-menu">
             <li><a href="#">Home</a></li>
             <li><a href="#">Organisasi</a></li>
             <li><a href="#">Publikasi</a></li>
             <li><a href="#">Login</a></li>
-
         </ul>
+    </nav>
     </div>
     <div class="container d-flex align-item-center"></div>
         <div class="breadcrumb">
-            <h1> Sistem informasi berbasis angka </h1>
+            <h2> Home / Organisasi OPD - Sikondang </h2>
         </div>
+        <h1> Organisasi OPD - Sikondang </h1>
+
+        <style>
+            body {
+              font-family: Arial;
+            }
+            
+            * {
+              box-sizing: border-box;
+            }
+            
+            form.example input[type=text] {
+              padding: 7px;
+              font-size: 17px;
+              border: 1px solid grey;
+              float: right;
+              width: 23%;
+              background: #f1f1f1;
+            }
+            
+            form.example button {
+              float: right;
+              width: 10%;
+              padding: 17px;
+              background: #2196F3;
+              color: white;
+              font-size: 16px;
+              border: 1px solid grey;
+              border-left: none;
+              cursor: pointer;
+            }
+            
+            form.example button:hover {
+              background: #0b7dda;
+            }
+            
+            form.example::after {
+              content: "";
+              clear: both;
+              display: table;
+            }
+            </style>
+            </head>
+            <body>
+            
+            
+            <form class="example" action="/action_page.php">
+              <input type="text" placeholder="Search.." name="search">
+              <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        
         <div class="title">Organisasi OPD - Sikondang</div>
         <div class="card-container">
             <div class="card">
-                <img src="c:\Users\user\Pictures\Saved Pictures\government.png">
+                <img src="c:\Users\user\Pictures\Saved Pictures\bidangserang.png">
                 <div class="card-title">Badan Kepegawaian Pengembangan Sumber Daya Manusia Kota Serang</div>
             </div>
             <div class="card">
-                <img src="c:\Users\user\Pictures\Saved Pictures\government.png">
+                <img src="c:\Users\user\Pictures\Saved Pictures\logo.jpg">
                 <div class="card-title">Badan Kesatuan Bangsa dan Politik Kota Serang</div>
             </div>
             <div class="card">
@@ -147,7 +200,34 @@
                 <img src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Serang_City_Logo.png" alt="Pemerintah Kota Serang">
                 <div class="card-title">Dinas Lingkungan Hidup Kota Serang</div>
             </div>
+            <div class="card">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Serang_City_Logo.png" alt="Pemerintah Kota Serang">
+                <div class="card-title">Dinas Lingkungan Hidup Kota Serang</div>
+            </div>
+            <div class="card">
+                 <img src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Serang_City_Logo.png" alt="Pemerintah Kota Serang">
+                 <div class="card-title">Dinas Lingkungan Hidup Kota Serang</div>
+            </div>
+            <div class="card">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Serang_City_Logo.png" alt="Pemerintah Kota Serang">
+                <div class="card-title">Dinas Lingkungan Hidup Kota Serang</div>
+           </div>
+           <div class="card">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Serang_City_Logo.png" alt="Pemerintah Kota Serang">
+                <div class="card-title">Dinas Lingkungan Hidup Kota Serang</div>
+       </div>
+
+           <div class="statistics">
+            <h3>Statistik Pengunjung</h3>
+            <p>Pengunjung Hari ini: 16 Orang</p>
+            <p>Pengunjung Bulan ini: 839 Orang</p>
+            <p>Pengunjung Tahun ini: 5028 Orang</p>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </body>
 </html>
